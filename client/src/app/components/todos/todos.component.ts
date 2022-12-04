7eb15a50aede51cc24db3181aae45c228b708102
z <ncxwork@gmail.com>
2022-12-04 23:28:21 +0800

    [ToDoComponent, Css] Add Task functionality + styling

--- client/src/app/components/todos/todos.component.ts
+++ client/src/app/components/todos/todos.component.ts
@@ -1,45 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../../models/Todo'
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-
   todos: Todo[] = [];
+  task: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-      this.todos = [
-        {
-          content: "First todo",
-          completed: false
-        },
-        {
-          content: "Second todo",
-          completed: true
-        },
-        {
-          content: "Third todo",
-          completed: false
-        },
-
-      ]
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false,
+      },
+      {
+        content: 'Second todo',
+        completed: true,
+      },
+      {
+        content: 'Third todo',
+        completed: false,
+      },
+    ];
   }
 
   toggleDone(id: number): void {
     this.todos.map((value, idx) => {
       if (idx == id) {
-        value.completed = !value.completed
+        value.completed = !value.completed;
       }
       return value;
-    })
+    });
   }
 
   deleteTodo(id: number): void {
     this.todos = this.todos.filter((value, idx) => idx !== id);
+  }
+
+  addTodo(): void {
+    this.todos = [...this.todos, { content: this.task, completed: false }];
+    this.task = '';
   }
 }
